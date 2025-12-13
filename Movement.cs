2256e3046ffdfdc95082a2110710d3143bf63aec
Jose Garcia <josegarcia19809@gmail.com>
2025-12-13 17:46:54 -0600

    Rocket Boost 3D: Rotation Input

--- Movement.cs
+++ Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] private InputAction thrust;
+    [SerializeField] private InputAction rotation;
     [SerializeField] private float thrustStrength = 1000f;
     Rigidbody rb;
 
@@ -16,14 +17,26 @@ public class Movement : MonoBehaviour
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
     void FixedUpdate()
     {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        Debug.Log(rotationInput);
+    }
+
+    private void ProcessThrust()
+    {
         if (thrust.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * (thrustStrength * Time.deltaTime));
-            Debug.Log(thrustStrength);
         }
     }
 }
