f717b9c7a6ac56f10ca226569462b07755776e65
Jose Garcia <josegarcia19809@gmail.com>
2025-12-13 16:48:38 -0600

    Rocket Boost 3D: Using AddRelativeForce()

--- Movement.cs
+++ Movement.cs
@@ -5,17 +5,25 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] private InputAction thrust;
+    [SerializeField] private float thrustStrength = 1000f;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void OnEnable()
     {
         thrust.Enable();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (thrust.IsPressed())
         {
-            print("Pressed");
+            rb.AddRelativeForce(Vector3.up * (thrustStrength * Time.deltaTime));
+            Debug.Log(thrustStrength);
         }
     }
 }
