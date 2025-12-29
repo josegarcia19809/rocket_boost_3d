9f004ae3245b4008cbe325499f98a93017aa7f44
Jose Garcia <josegarcia19809@gmail.com>
2025-12-29 07:16:54 -0600

    Rocket Boost 3D: Oscillating Object Script

---
+++ Oscillator.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] float speed;
+    float movementFactor;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        endPosition = startPosition + movementVector;
+    }
+
+    void Update()
+    {
+        movementFactor = Mathf.PingPong(Time.time * speed, 1f);
+        transform.position = Vector3.Lerp(startPosition, endPosition, movementFactor);
+    }
+}
